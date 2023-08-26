0fe5c2d602783489fae5f21345ad5bca6d41a09f
Bashirli <bashirli1903@gmail.com>
2023-08-26 17:49:05 +0400

    finish

---
+++ app/src/main/java/com/bashirli/mymovie/common/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.bashirli.mymovie.common.util
+const val BASE_IMAGE_URL=""
+const val BASE_URL=""
+const val API_KEY=""
+const val LIMIT_ITEM=4
+const val BASE_SP=""
+const val TOKEN=""
+const val DATE_FORMAT="dd/MM/yyyy"
